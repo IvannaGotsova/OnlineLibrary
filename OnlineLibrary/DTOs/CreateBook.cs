a2c55972692141e5ce3625d184724bdf1568febb
IvannaGotsova <ivannagotsova@outlook.com>
2025-08-27 17:31:33 +0300

    Created CreateBook class

---
+++ OnlineLibrary/DTOs/CreateBook.cs
@@ -0,0 +1,14 @@
+﻿namespace OnlineLibrary.DTOs
+{
+    public class CreateBook
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Author { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public Genre Genre { get; set; }
+        public int Pages { get; set; }
+        public decimal Price { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
