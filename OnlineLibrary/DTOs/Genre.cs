bae880873f25e6c6b9de3c16154c76b4e28a917c
IvannaGotsova <ivannagotsova@outlook.com>
2025-08-27 17:31:45 +0300

    Created enum Genre

---
+++ OnlineLibrary/DTOs/Genre.cs
@@ -0,0 +1,22 @@
+﻿namespace OnlineLibrary.DTOs
+{
+    public enum  Genre
+    {
+        Autobiography, 
+        Biography, 
+        Fantasy, 
+        HistoricalFiction,
+        History, 
+        Horror,
+        LiteraryFiction, 
+        Memoir, 
+        Mystery, 
+        Philosophy, 
+        Romance, 
+        ScienceNature, 
+        ScienceFiction, 
+        SelfHelp, 
+        Thriller, 
+        TrueCrime
+    }
+}
