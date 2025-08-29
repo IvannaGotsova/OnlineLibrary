729b1f060bfbd14c45a1477a6fc32c1c504508ac
IvannaGotsova <ivannagotsova@outlook.com>
2025-08-29 11:20:44 +0300

    Added Extension and Context

--- OnlineLibrary/Program.cs
+++ OnlineLibrary/Program.cs
@@ -1,6 +1,15 @@
+using OnlineLibrary.DBContext;
+using OnlineLibrary.Logic;
+using System;
+using  static OnlineLibrary.Logic.BookMapEndpointsExtensions;
+
 var builder = WebApplication.CreateBuilder(args);
+
+var connectionString = "Data Sourse=OnlineLibrary.db";
+builder.Services.AddSqlite<OnlineLibraryContext>(connectionString);
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.BookMapEndpointsExt();
 
 app.Run();    
