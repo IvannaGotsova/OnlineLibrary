61c8960c90450a14de39ab93429f403b2d2b06f6
IvannaGotsova <ivannagotsova@outlook.com>
2025-08-31 09:59:39 +0300

    Added endpoints and dataseeder

--- OnlineLibrary/Program.cs
+++ OnlineLibrary/Program.cs
@@ -1,15 +1,26 @@
+using Microsoft.EntityFrameworkCore;
+using OnlineLibrary.Data;
 using OnlineLibrary.DBContext;
 using OnlineLibrary.Logic;
 using System;
 using  static OnlineLibrary.Logic.BookMapEndpointsExtensions;
+using static OnlineLibrary.Logic.AuthorMapEndpointsExtensions;
+using static OnlineLibrary.Logic.UserMapEndpointsExtensions;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
-var connectionString = "Data Sourse=OnlineLibrary.db";
-builder.Services.AddSqlite<OnlineLibraryContext>(connectionString);
+builder.Services.AddDbContext<OnlineLibraryContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 
 var app = builder.Build();
 
+DatabaseSeed.DatabaseSeeder(app);
+
+app.HomeMapEndpointsExt();
+app.AuthorMapEndpointsExt();
+app.UserMapEndpointsExt();
 app.BookMapEndpointsExt();
 
 app.Run();    
