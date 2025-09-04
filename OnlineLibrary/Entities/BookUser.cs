3aa1405cd9ae067321f52391d8c30f9ca9b89167
IvannaGotsova <ivannagotsova@outlook.com>
2025-09-04 10:08:32 +0300

    Added new entity

---
+++ OnlineLibrary/Entities/BookUser.cs
@@ -0,0 +1,11 @@
+﻿namespace OnlineLibrary.Entities
+{
+    public class BookUser
+    {
+        public int UserId { get; set; }
+        public User User { get; set; }
+
+        public int BookId { get; set; }
+        public Book Book { get; set; }
+    }
+}
